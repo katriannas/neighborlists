8b51aace0ab4a7ffd2b4d85318d2d5018e695bee
katriannas <katrianna.sarkar@emory.edu>
2025-01-29 05:14:50 -0500

    Update randompoints-dspheres.py

--- randomgenerators/randompoints-dspheres.py
+++ randomgenerators/randompoints-dspheres.py
@@ -1,23 +1,21 @@
-#Script to generate random configuration of N monodisperse d-spherical particles within a unit cell
+#Generate random configuration of N monodisperse d-spherical particles
 
 import numpy as np
 import sys
 
-#Input dimension, number of particles, and radius of particles as command-line arguments
+# Input dimension, number of particles, and radius of particles as command-line arguments
 d = int(sys.argv[1])
 n = int(sys.argv[2])
 r = float(sys.argv[3])
 
-#Create an array of points for particle centers
+# Generate points (centers of the spheres)
 points = np.random.rand(n, d)
 
-#Create an array to store these points and the radius around each point
-#The "radii" vector extends in all directions around the center of the particle, creating a d-sphere
-radii = np.full((n, d), r)
+# Combine points and radius into a single array for saving
+radii = np.full((n, 1), r)  # Single radius per particle
 particles = np.hstack((points, radii))
 
-#Save all of this to a text file
-#Dimension, number of particles, and radius as strings for the filename
+# Create a text file and save the array to the file
 dimension = sys.argv[1]
 number = sys.argv[2]
 radius = sys.argv[3]
